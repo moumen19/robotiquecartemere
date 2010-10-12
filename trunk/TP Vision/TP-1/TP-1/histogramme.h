72bec97a258a7490b13840ebc3541434f665de6e
olivier.jacquel <olivier.jacquel@308fe9b9-b635-520e-12eb-2ef3f824b1b6>
2010-10-12 13:21:33 +0000

    voilà le TP1

---
+++ trunk/TP Vision/TP-1/TP-1/histogramme.h
@@ -0,0 +1,24 @@
+#ifndef HISTOGRAMS_H_INCLUDED
+#define HISTOGRAMS_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <cv.h>
+#include <highgui.h>
+#include <cvaux.h>
+#include <cxcore.h>
+
+
+void compute_histo_16(char * img_name, int * histo_16xRGB);
+
+void compute_histo_256(char * img_name);
+
+int intersection_histo(int * histo1, int * histo2, int histoSize);
+
+void greylevel(IplImage* img[0]);
+
+void reduce_greylevel(IplImage* img[0], int level);
+
+#endif // HISTOGRAMS_H_INCLUDED
---
+++ trunk/TP Vision/TP-1/TP-1/intersection.c
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////
+//
+// hello-world.cpp
+//
+// This is a simple, introductory OpenCV program. The program reads an
+// image from a file and displays the result.
+//
+////////////////////////////////////////////////////////////////////////
+#include "histogramme.h"
+
+// D:\TRAVAIL\M2\Robotique Avancée\Vision\TPs\TP_Vision_1\bin\Debug\TP_Vision_1.exe test.jpg lena.jpg
+
+int main(int argc, char *argv[])
+{
+
+  int histo_RGB16_1[48];
+  int histo_RGB16_2[48];
+
+  // verification des arguments en ligne de commande
+  if(argc<3)
+  {
+    printf("2 images a mettre en argument");
+    exit(0);
+  }
+
+  compute_histo_16(argv[1],histo_RGB16_1);
+  compute_histo_16(argv[2],histo_RGB16_2);
+
+  intersection_histo(histo_RGB16_1, histo_RGB16_2, 48);
+
+  system("pause");
+
+  return 0;
+}
