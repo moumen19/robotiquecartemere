1b23d1e71934e9f51fe9128ca77a6631f31a2682
ferdinand.piette <ferdinand.piette@308fe9b9-b635-520e-12eb-2ef3f824b1b6>
2010-12-02 19:35:25 +0000

    Commentaire Debug et BusRS232

--- trunk/code source/Debug.hpp
+++ trunk/code source/Debug.hpp
@@ -16,7 +16,6 @@
     // Inclusion des bibliothèques
     #include <iostream>
     #include <vector>
-    //#include <pthread.h>
     #include <boost/thread/mutex.hpp>
     #include <sstream>
 
@@ -39,17 +38,13 @@
                                                     _DEBUG::unlock(); \
                                                 }
 
-    /*#define _DEBUG(title, message, priority)    if(_DEBUG_MODE == true) \
-                                                { \
-                                                    throw Message(title, message, priority); \
-                                                }*/
-
     // Définition d'une macro exécutant une méthode statique
     #define _DEBUG_EXEC(f)  if(_DEBUG_MODE == true) \
                                 _DEBUG::f;
 
     // Définition des constantes de configurations
     typedef int Debug_Configuration;
+    #define DISPLAY_NONE 0
     #define CONSOLE_DISPLAY 1
     #define FILE_DISPLAY 2
 
@@ -67,42 +62,28 @@
         std::string title;          // Titre du message
         std::string content;        // Contenu du message
         Debug_Priority priority;    // Priorité du message
-        bool isEmpty;               // Définie si le message à ubn sens
+        bool isEmpty;               // Définie si le message à un sens
     } typedef Message;
-
-    /*class Message : public std::exception
-    {
-        public:
-            Message(std::string , std::string , Debug_Priority) throw();
-            virtual ~Message() throw();
-            virtual const char * what()const throw();
-
-        private:
-            std::string a_title;          // Titre du message
-            std::string a_content;        // Contenu du message
-            Debug_Priority a_priority;    // Priorité du message
-    };*/
 
     // Classe de débuggage
     class _DEBUG
     {
         public:
-            static void addMessage(std::string, std::string, Debug_Priority);
-            static void clearAllMessage();
-            static void configuration(Debug_Configuration, std::string);
-            static Message getMessage(unsigned int);
-            static Message getNextMessage();
-            static int getNumMessages();
-            static void lock();
-            static void unlock();
+            static void addMessage(std::string, std::string, Debug_Priority);	// Ajoute un message
+            static void clearAllMessage();					// Efface tous les messages
+            static void configuration(Debug_Configuration, std::string);	// Configure le mode Debug
+            static Message getMessage(unsigned int);				// Retourne un message
+            static Message getNextMessage();					// Retourne le message suivant
+            static int getNumMessages();					// Donne le nombre de message stocké
+            static void lock();							// Verouille un mutex
+            static void unlock();						// Deverouille le mutex
 
         private:
-            static std::vector<Message> a_messages;
-            static unsigned int a_cursor;
-            static Debug_Configuration a_configuration;
-            static std::string a_filename;
-            //static pthread_mutex_t a_mutex;
-	    static boost::mutex a_mutex;
+            static std::vector<Message> a_messages;				// Stockage des messages
+            static unsigned int a_cursor;					// Curseur indiquant quel sera le prochain message retourne par getNextMessage()
+            static Debug_Configuration a_configuration;				// Configuration du mode Debug
+            static std::string a_filename;					// Chemin du fichier de logs si existant
+	    static boost::mutex a_mutex;					// Mutex de protection
 
     };
 
