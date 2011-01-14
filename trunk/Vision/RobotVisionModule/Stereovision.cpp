c375d5d34f7f28ba45d785ac6091f2072d8079df
julien.delbergue <julien.delbergue@308fe9b9-b635-520e-12eb-2ef3f824b1b6>
2011-01-14 15:37:32 +0000

    qq modif dans la prise de notes vision

--- trunk/Vision/RobotVisionModule/Stereovision.cpp
+++ trunk/Vision/RobotVisionModule/Stereovision.cpp
@@ -18,11 +18,13 @@ void Stereovision::Setup()
     try{
         // setup left camera
         m_LeftCamera = new Camera("webcam1.avi");
+        //m_LeftCamera = new Camera(0);
         if(!m_LeftCamera->isOpened())
            throw (new std::exception());
 
         // setup right camera
         m_RightCamera = new Camera("webcam2.avi");
+        //m_RightCamera = new Camera(1);
         if(!m_RightCamera->isOpened())
             throw (new std::exception());
 
@@ -36,34 +38,86 @@ void Stereovision::Setup()
 // display both cameras untouched
 void Stereovision::RawVideoDisplay()
 {
-    cv::Mat frame;
+    cv::Mat frameL, frameR;
     cv::namedWindow( "rawDisplay_Left", CV_WINDOW_AUTOSIZE);
     cv::namedWindow( "rawDisplay_Right", CV_WINDOW_AUTOSIZE);
-    char c = 0;
 
-    while(c<1)
+    while(cv::waitKey(20) < 1)
     {
-        c = (char)cv::waitKey(2); // shortest possible delay(experience result)
-
         // left
-        *m_LeftCamera >> frame;
-
-        if(frame.empty()) break;
-
-        cv::imshow( "rawDisplay_Left", frame );
+        *m_LeftCamera >> frameL;
+        if(frameL.empty()) break;
 
         // right
-        *m_RightCamera >> frame;
+        *m_RightCamera >> frameR;
+        if(frameR.empty())break;
 
-        if(frame.empty())break;
-
-        cv::imshow( "rawDisplay_Right", frame );
+        cv::imshow( "rawDisplay_Left", frameL );
+        cv::imshow( "rawDisplay_Right", frameR );
     }
 }
 
 // main routine of the video processing module
 void Stereovision::Run()
 {
-    RawVideoDisplay();
+    cv::Mat frameL, frameR;
+    cv::namedWindow( "rawDisplay_Left", CV_WINDOW_AUTOSIZE);
+    cv::namedWindow( "rawDisplay_Right", CV_WINDOW_AUTOSIZE);
+
+    while(cv::waitKey(20) < 1)
+    {
+        // left
+        *m_LeftCamera >> frameL;
+        if(frameL.empty()) break;
+
+        // right
+        *m_RightCamera >> frameR;
+        if(frameR.empty())break;
+
+        cv::imshow( "rawDisplay_Left", frameL );
+        cv::imshow( "rawDisplay_Right", frameR );
+    }
+}
+
+void Stereovision::test()
+{
+    cv::Mat img1raw, img2raw;
+    cv::Mat img1, img2;
+
+    // left
+    *m_LeftCamera >> img1raw;
+    *m_RightCamera>> img2raw;
+
+
+    cv::cvtColor(img1raw, img1, CV_BGR2GRAY);
+    cv::cvtColor(img2raw, img2, CV_BGR2GRAY);
+
+    // detecting keypoints
+    cv::FastFeatureDetector detector;
+    //cv::SiftFeatureDetector detector;
+	//cv::SurfFeatureDetector detector(10000);
+	vector<cv::KeyPoint> keypoints1, keypoints2;
+	detector.detect(img1, keypoints1);
+	detector.detect(img2, keypoints2);
+
+	// computing descriptors
+	cv::SurfDescriptorExtractor extractor;
+	//cv::SiftDescriptorExtractor extractor;
+	//cv::SurfDescriptorExtractor extractor;
+	cv::Mat descriptors1, descriptors2;
+	extractor.compute(img1, keypoints1, descriptors1);
+	extractor.compute(img2, keypoints2, descriptors2);
+
+	// matching descriptors
+	cv::BruteForceMatcher<cv::L2<float> > matcher;
+    vector<cv::DMatch> matches;
+    matcher.match(descriptors1, descriptors2, matches);
+
+	// drawing the results
+	cv::namedWindow("matches", 1);
+	cv::Mat img_matches;
+	cv::drawMatches(img1, keypoints1, img2, keypoints2, matches, img_matches);
+	cv::imshow("matches", img_matches);
+	cv::waitKey(0);
 
 }
