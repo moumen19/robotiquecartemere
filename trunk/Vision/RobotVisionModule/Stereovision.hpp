65fefc62c87c7bd5c636e7cc9268404f9e5f2913
morgan.bouve@gmail.com <morgan.bouve@gmail.com@308fe9b9-b635-520e-12eb-2ef3f824b1b6>
2011-01-26 23:54:04 +0000

    Calibration

--- trunk/Vision/RobotVisionModule/Stereovision.hpp
+++ trunk/Vision/RobotVisionModule/Stereovision.hpp
@@ -1,14 +1,19 @@
 #ifndef STEREOVISION_H
 #define STEREOVISION_H
 
+
+#include <string>
 #include <iostream>
+#include <fstream>
+
 
 #include "Camera.hpp"
 #include "GenericImageProcessing.hpp"
 
 // for previous functions
 #include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2/highgui/highgui.hpp>
+
 
 #define CAMERA_MODE 0
 #define VIDEO_FILE_MODE 1
@@ -23,7 +28,9 @@ class Stereovision
 
         // To implement
         virtual void Send(){;}
-        void CalibrateStereo () {;}
+        void StereoCalibrate();
+        void SaveMatrix();
+        void LoadMatrix(const string &filename, const string &filename2);
 
         // Mandatory
         void Setup(int mode);
@@ -53,6 +60,23 @@ class Stereovision
         Camera * m_RightCamera;
         cv::Mat m_LeftFrame;
         cv::Mat m_RightFrame;
+
+        cv::Mat m_rotationMatrix;
+        cv::Mat m_translationMatrix;
+        cv::Mat m_essentialMatrix;
+        cv::Mat m_fundamentalMatrix;
+
+        cv::Mat m_intrinsecMatrix1;
+        cv::Mat m_distortionMatrix1;
+        cv::Mat m_object_points;
+        cv::Mat m_image_points1;
+        cv::Size m_board_sz;
+
+
+        cv::Mat m_intrinsecMatrix2;
+        cv::Mat m_distortionMatrix2;
+        cv::Mat m_image_points2;
+
 
         // optional features
         vector<cv::Mat > m_LeftImageBuffer;
